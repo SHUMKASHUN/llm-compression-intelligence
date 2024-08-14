11bd2070c06295cdcbc4c2a6c824e3570e12bbe1
SHUM Ka Shun <ksshumab@jxcpu1.cse.ust.hk>
2024-08-14 11:22:12 +0800

    update script

--- code/evaluation/run.sh
+++ code/evaluation/run.sh
@@ -1,9 +1,26 @@
 
-CUDA_VISIBLE_DEVICES=0 python -u main.py\
-    --model_name huggyllama/llama-7b\
+# CUDA_VISIBLE_DEVICES=0 python -u main.py\
+#     --model_name huggyllama/llama-7b\
+#     --task_name cc\
+#     --block_size 1900\
+#     --stride 512\
+#     --batch_size 1\
+#     --cluster 0 \
+#     --flash 
+export HF_ENDPOINT=https://hf-mirror.com
+model=$1
+device=$2
+begin=$3
+end=$4
+
+for i in $( eval echo {$begin..$end} )
+do
+CUDA_VISIBLE_DEVICES=$device python -u main.py\
+    --model_name $model\
     --task_name cc\
     --block_size 1900\
-    --stride 512\
+    --stride 512 \
     --batch_size 1\
-    --cluster 0 \
-    --flash 
+    --cluster $i \
+    --flash
+done
